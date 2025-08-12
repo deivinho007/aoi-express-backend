82ae1ec482758927a9068e4eb2dcda712932dcb3
deivid santana <deivid.santana@aluno.>
2025-08-12 17:48:47 -0300

    cria o crud do profile

--- src/Server.js
+++ src/Server.js
@@ -1,11 +1,39 @@
-import express from'express'
+import express from 'express'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.get('/profile',(req,res)=>{
+  const dados = req.body
+  console.log(req)
+  res.json({messsage:'Perfil do Usuário'})
 })
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta  http://localhost: ${port}`)
+app.post('/profile', (req, res) => {
+  const dados = req.body;
+  res.json({
+    message: 'Usuario Criado',
+    profile: dados
+  });
+});
+
+
+app.put('/profile',(req,res)=>{
+const dados = req.body
+  res.json({
+    messsage:'Usuario atualizado ',
+    profile: dados
+  })
+})
+
+app.delete('/profile',(req,res)=>{
+  console.log(req)
+  res.json({messsage:'Usuario deletado'})
+})
+
+
+app.listen(port,function(){
+  console.log(`Servidor rodando na porta ${port}`)
 })
